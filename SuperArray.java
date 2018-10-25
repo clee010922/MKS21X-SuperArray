f50230ef888be9a91ff4cfeccabf440bee6ba28a
clee010922 <clee01@stuy.edu>
2018-10-25 19:45:26 -0400

    edited code for phase 1 and 2

--- SuperArray.java
+++ SuperArray.java
@@ -10,46 +10,45 @@ public class SuperArray {
   }
 
   public void clear() {
-    size = 0;
+    this.data = new String[(this.data).length];
+    this.size = 0;
   }
 
   public int size() {
-    return size;
+    return this.size;
   }
 
   public boolean isEmpty() {
-    return size == 0;
+    return this.size == 0;
   }
 
   public boolean add(String word) {
-    if (data[size - 1] != null)
+    if (data.length == this.size)
       resize();
-    data[size-1] = word;
-    size++;
+    this.data[this.size] = word;
+    this.size++;
     return true;
   }
 
   public String toString() {
     String a = "[";
-    for(int i = 0; i < size-1; i++) {
-      if (data[i] == null)
-        a += "";
-      else a += data[i] + ", ";
-    }
-    if (data[size] == null)
-      a += "]";
-    else a += data[size] + "]";
-    return a;
-  }
+    for(int i = 0; i < size; i++) {
+        a += this.data[i];
+        if (i != size - 1) {
+        a += ", ";
+        }
+        }
+        return a += "]";
+      }
 
   public String toStringDebug() {
     String a = "[";
-    for(int i = 0; i <  data.length()-1; i++) {
+    for(int i = 0; i < data.length - 1; i++) {
       if (data[i] == null)
         a += "null, ";
       else a += data[i] + ", ";
     }
-    if (data.length() == null)
+    if (data[data.length-1] == null)
       a += "null]";
     else a+= data[size] + "]";
     return a;
