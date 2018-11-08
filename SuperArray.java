80640b1327509de6335f694c215b153df8db7be8
clee010922 <clee01@stuy.edu>
2018-11-07 21:49:20 -0500

    edited syntax for the entire code: deleted unnecessary 'this', edited add so when indexOf is used, it doesnt point to a null anymore, and same for lastIndexOf. Tested with a new driver, everything works now

--- SuperArray.java
+++ SuperArray.java
@@ -6,41 +6,38 @@ public class SuperArray {
 
   public SuperArray() {
     data = new String[10];
-    size = 0;
   }
 
   public SuperArray(int startingCapacity) {
     if (startingCapacity < 0)
       throw new IllegalArgumentException(startingCapacity + "is an invalid size");
     data = new String[startingCapacity];
-    size = 0;
   }
 
   public void clear() {
-    this.data = new String[(this.data).length];
-    this.size = 0;
+    size = 0;
   }
 
   public int size() {
-    return this.size;
+    return size;
   }
 
   public boolean isEmpty() {
-    return this.size == 0;
+    return size == 0;
   }
 
   public boolean add(String word) {
-    if (data.length == this.size)
+    if (data.length == size)
       resize();
-    this.data[this.size] = word;
-    this.size++;
+    data[size] = word;
+    size++;
     return true;
   }
 
   public String toString() {
     String a = "[";
-    for(int i = 0; i < this.size; i++) {
-      a += this.data[i];
+    for(int i = 0; i < size; i++) {
+      a += data[i];
       if (i != size - 1)
         a += ", ";
       }
@@ -49,8 +46,8 @@ public class SuperArray {
 
   public String toStringDebug() {
     String a = "[";
-    for(int i = 0; i < this.data.length - 1; i++) {
-      a += this.data[i];
+    for(int i = 0; i < data.length - 1; i++) {
+      a += data[i];
       if (i != size - 1)
         a += ", ";
       }
@@ -60,23 +57,23 @@ public class SuperArray {
   public String get(int index) {
     if (index < 0 || index >= size())
       throw new IndexOutOfBoundsException(index + "is an invalid index");
-    else return this.data[index];
+    else return data[index];
   }
 
   public String set(int index, String word) {
     String old = "";
     if (index < 0 || index >= size())
       throw new IndexOutOfBoundsException(index + "is an invalid index");
-    else this.data[index] = word;
+    else data[index] = word;
     return old;
     }
 
     private void resize() {
-      String[] copy = new String[this.size * 2 + 1];
+      String[] copy = new String[size * 2 + 1];
       for (int i = 0; i < size; i++) {
-        copy[i] = this.data[i];
+        copy[i] = data[i];
       }
-      this.data = copy;
+      data = copy;
     }
 
     public boolean contains(String word) {
@@ -88,8 +85,8 @@ public class SuperArray {
     }
 
   public int indexOf(String word) {
-    for (int i = 0; i < data.length; i++) {
-      if (data[i] != null && data[i].equals(word))
+    for (int i = 0; i < size; i++) {
+      if (data[i].equals(word))
         return i;
     }
     return -1;
@@ -97,43 +94,57 @@ public class SuperArray {
 
   public int lastIndexOf(String word) {
     for (int i = size - 1; i > 0; i--) {
-      if (word.equals(data[i]))
+      if (data[i].equals(word))
         return i;
     }
     return -1;
   }
 
   public void add(int index, String word) {
-    if (index < 0 || index > size())
-      throw new IndexOutOfBoundsException(index + "is an invalid index");
-    if (this.size == data.length)
+    if (index < 0 || index > size()) {
+    throw new IndexOutOfBoundsException();
+  }
+    else {
+      if (data.length == size) {
       resize();
-    for (int i = index; i < this.size - 1; i++) {
-      this.data[i + 1] = this.data[i];
     }
-    this.data[index] = word;
-    size++;
+      String[] result = new String[data.length];
+      for (int i = 0; i < size; i++) {
+        result[i] = data[i];
+      }
+      for (int i = index; i < size; i++) {
+        result[i + 1] = data[i];
+      }
+      result[index] = word;
+      size++;
+      data = result;
+    }
   }
 
   public String remove(int index) {
-    String old = this.data[index];
+    String removed = data[index];
     if (index < 0 || index > size())
-      throw new IndexOutOfBoundsException(index + "is an invalid index");
-    for (int i = index; i < this.size; i++) {
-      this.data[i] = this.data[i + 1];
-      if (this.data[i + 1] == null)
-        data[i] = null;
+    {
+      throw new IndexOutOfBoundsException();
+  }
+      String[] result = new String[data.length];
+      for (int i = 0; i < size(); i++) {
+        result[i] = data[i];
       }
-    size--;
-    return old;
+      for (int i = index; i < size() - 1; i++) {
+        result[i] = result[i + 1];
+      }
+      size--;
+      data = result;
+    return removed;
   }
 
   public boolean remove(String word) {
-    if (this.contains(word)) {
-      this.remove(this.indexOf(word));
+    if (contains(word)) {
+      remove(indexOf(word));
       return true;
     }
-    return (this.contains(word));
+    return (contains(word));
   }
 
 
